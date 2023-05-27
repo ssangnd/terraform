3dccc7730c337cef089ce991d417eb05be7d0ded
Sang Nguyen <75923274+ssangnd@users.noreply.github.com>
2023-05-27 08:29:09 +0700

    init/config terraform

--- main.tf
+++ main.tf
@@ -2,8 +2,19 @@ provider "aws" {
   region = "us-east-1"
 }
 
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  owners = ["099720109477"] # Canonical Ubuntu AWS account id
+}
+
 resource "aws_instance" "hello" {
-  ami           = "ami-0715c1897453cabd1"
+  ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
   tags = {
     Name = "HelloWorld"
