efec35d9ebf52ca51215db81e084623b251a65c4
Sang Nguyen <75923274+ssangnd@users.noreply.github.com>
2023-05-27 09:33:11 +0700

    create s3 bucket

--- main.tf
+++ main.tf
@@ -1,22 +1,11 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "us-west-2"
 }
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
+resource "aws_s3_bucket" "terraform-bucket" {
+  bucket = "terraform-series-bucket"
 
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-
-  owners = ["099720109477"] # Canonical Ubuntu AWS account id
-}
-
-resource "aws_instance" "hello" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = "t2.micro"
   tags = {
-    Name = "HelloWorld"
+    Name        = "Terraform Series"
   }
 }
