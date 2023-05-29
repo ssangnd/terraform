0bc6c55ba1d22c72315f22063083cfe475817c28
Sang Nguyen <75923274+ssangnd@users.noreply.github.com>
2023-05-29 10:15:26 +0700

    s3 web

--- main.tf
+++ main.tf
@@ -1,11 +1,78 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.44.0"
+    }
+  }
+}
+
 provider "aws" {
   region = "us-west-2"
 }
 
-resource "aws_s3_bucket" "terraform-bucket" {
-  bucket = "terraform-series-bucket"
-
-  tags = {
-    Name        = "Terraform Series"
+resource "aws_s3_bucket" "static" {
+  bucket        = "terraform-series-bai3sangnd"
+  force_destroy = true
+}
+# 
+resource "aws_s3_bucket_ownership_controls" "static" {
+  bucket = aws_s3_bucket.static.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
   }
+}
+
+resource "aws_s3_bucket_public_access_block" "static" {
+  bucket = aws_s3_bucket.static.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_acl" "static" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.static,
+    aws_s3_bucket_public_access_block.static,
+  ]
+
+  bucket = aws_s3_bucket.static.id
+  acl    = "public-read"
+}
+# 
+
+resource "aws_s3_bucket_website_configuration" "static" {
+  bucket = aws_s3_bucket.static.bucket
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
+}
+
+resource "aws_s3_bucket_policy" "static" {
+  bucket = aws_s3_bucket.static.id
+  policy = file("s3_static_policy.json")
+}
+
+data "aws_iam_policy_document" "static" {
+  statement {
+    actions   = ["s3:GetObject"]
+    resources = ["${aws_s3_bucket.static.arn}/*"]
+
+    principals {
+      type = "*"
+      identifiers = ["*"]
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "static" {
+  bucket = aws_s3_bucket.static.id
+  policy = data.aws_iam_policy_document.static.json
 }
