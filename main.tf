eaac0eae93476da1cc724c3e9f5831f0499d058b
Sang Nguyen <75923274+ssangnd@users.noreply.github.com>
2023-05-31 11:08:36 +0700

    using module terraform

--- main.tf
+++ main.tf
@@ -11,14 +11,23 @@ provider "aws" {
   region = "us-west-2"
 }
 
-resource "aws_vpc" "vpc" {
-  cidr_block           = "10.0.0.0/16"
-  enable_dns_hostnames = true
+module "vpc" {
+  source = "./vpc"
 
-  tags = {
-    "Name" = "custom"
-  }
+  vpc_cidr_block    = "10.0.0.0/16"
+  private_subnet    = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnet     = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+  availability_zone = ["us-west-2a", "us-west-2b", "us-west-2c"]
 }
+
+# resource "aws_vpc" "vpc" {
+#   cidr_block           = "10.0.0.0/16"
+#   enable_dns_hostnames = true
+
+#   tags = {
+#     "Name" = "custom"
+#   }
+# }
 
 # resource "aws_subnet" "private_subnet_2a" {
 #   vpc_id            = aws_vpc.vpc.id
@@ -50,92 +59,92 @@ resource "aws_vpc" "vpc" {
 #   }
 # }
 
-locals {
-  private = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public  = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
-  zone    = ["us-west-2a", "us-west-2b", "us-west-2c"]
-}
-resource "aws_subnet" "private_subnet" {
-  count = length(local.private)
+# locals {
+#   private = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+#   public  = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+#   zone    = ["us-west-2a", "us-west-2b", "us-west-2c"]
+# }
+# resource "aws_subnet" "private_subnet" {
+#   count = length(local.private)
 
-  vpc_id            = aws_vpc.vpc.id
-  cidr_block        = local.private[count.index]
-  availability_zone = local.zone[count.index % length(local.zone)]
+#   vpc_id            = aws_vpc.vpc.id
+#   cidr_block        = local.private[count.index]
+#   availability_zone = local.zone[count.index % length(local.zone)]
 
-  tags = {
-    "Name" = "private-subnet"
-  }
-}
+#   tags = {
+#     "Name" = "private-subnet"
+#   }
+# }
 
-resource "aws_subnet" "public_subnet" {
-  count = length(local.public)
+# resource "aws_subnet" "public_subnet" {
+#   count = length(local.public)
 
-  vpc_id            = aws_vpc.vpc.id
-  cidr_block        = local.public[count.index]
-  availability_zone = local.zone[count.index % length(local.zone)]
+#   vpc_id            = aws_vpc.vpc.id
+#   cidr_block        = local.public[count.index]
+#   availability_zone = local.zone[count.index % length(local.zone)]
 
-  tags = {
-    "Name" = "public-subnet"
-  }
-}
+#   tags = {
+#     "Name" = "public-subnet"
+#   }
+# }
 
-resource "aws_internet_gateway" "ig" {
-  vpc_id = aws_vpc.vpc.id
+# resource "aws_internet_gateway" "ig" {
+#   vpc_id = aws_vpc.vpc.id
 
-  tags = {
-    "Name" = "custom"
-  }
-}
+#   tags = {
+#     "Name" = "custom"
+#   }
+# }
 
-resource "aws_route_table" "public" {
-  vpc_id = aws_vpc.vpc.id
+# resource "aws_route_table" "public" {
+#   vpc_id = aws_vpc.vpc.id
 
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.ig.id
-  }
+#   route {
+#     cidr_block = "0.0.0.0/0"
+#     gateway_id = aws_internet_gateway.ig.id
+#   }
 
-  tags = {
-    "Name" = "public"
-  }
-}
+#   tags = {
+#     "Name" = "public"
+#   }
+# }
 
-resource "aws_route_table_association" "public_association" {
-  for_each       = { for k, v in aws_subnet.public_subnet : k => v }
-  subnet_id      = each.value.id
-  route_table_id = aws_route_table.public.id
-}
+# resource "aws_route_table_association" "public_association" {
+#   for_each       = { for k, v in aws_subnet.public_subnet : k => v }
+#   subnet_id      = each.value.id
+#   route_table_id = aws_route_table.public.id
+# }
 
-resource "aws_eip" "nat" {
-  vpc = true
-}
+# resource "aws_eip" "nat" {
+#   vpc = true
+# }
 
-resource "aws_nat_gateway" "public" {
-  depends_on = [aws_internet_gateway.ig]
+# resource "aws_nat_gateway" "public" {
+#   depends_on = [aws_internet_gateway.ig]
 
-  allocation_id = aws_eip.nat.id
-  subnet_id     = aws_subnet.public_subnet[0].id
+#   allocation_id = aws_eip.nat.id
+#   subnet_id     = aws_subnet.public_subnet[0].id
 
-  tags = {
-    Name = "Public NAT"
-  }
-}
+#   tags = {
+#     Name = "Public NAT"
+#   }
+# }
 
-resource "aws_route_table" "private" {
-  vpc_id = aws_vpc.vpc.id
+# resource "aws_route_table" "private" {
+#   vpc_id = aws_vpc.vpc.id
 
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_nat_gateway.public.id
-  }
+#   route {
+#     cidr_block = "0.0.0.0/0"
+#     gateway_id = aws_nat_gateway.public.id
+#   }
 
-  tags = {
-    "Name" = "private"
-  }
-}
+#   tags = {
+#     "Name" = "private"
+#   }
+# }
 
-resource "aws_route_table_association" "public_private" {
-  for_each       = { for k, v in aws_subnet.private_subnet : k => v }
-  subnet_id      = each.value.id
-  route_table_id = aws_route_table.private.id
-}
+# resource "aws_route_table_association" "public_private" {
+#   for_each       = { for k, v in aws_subnet.private_subnet : k => v }
+#   subnet_id      = each.value.id
+#   route_table_id = aws_route_table.private.id
+# }
